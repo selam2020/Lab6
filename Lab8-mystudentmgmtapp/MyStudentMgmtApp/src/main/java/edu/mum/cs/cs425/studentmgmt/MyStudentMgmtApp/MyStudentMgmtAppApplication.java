1faa6b517aa670ee0c97b8c46af743161d2ceac8
selam2020 <55061854+selam2020@users.noreply.github.com>
2020-05-30 15:07:37 -0500

    lab-8 commit

---
+++ Lab8-mystudentmgmtapp/MyStudentMgmtApp/src/main/java/edu/mum/cs/cs425/studentmgmt/MyStudentMgmtApp/MyStudentMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.studentmgmt.MyStudentMgmtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudentMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStudentMgmtAppApplication.class, args);
+	}
+
+}
